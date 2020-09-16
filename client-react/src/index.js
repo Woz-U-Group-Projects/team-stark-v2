28afedaab0806a438ce4b9d841d252743dcbd976
Sheila England <sh3ng1and@gmail.com>
2020-09-16 12:07:07 -0400

    react html & css display application name success!

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    React.createElement('h1', {}, 'Bill Payment and Budget Tool'),
+    document.getElementById('root')
+  );
+  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
