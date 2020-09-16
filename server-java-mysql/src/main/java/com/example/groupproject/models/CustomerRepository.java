96c801bb5f0d1999766fcdb489a076f48acf63e0
Sheila England <sh3ng1and@gmail.com>
2020-09-16 17:25:47 -0400

    created customer controller; added findByCustomerid to customer repository interface; added table to customer class

--- server-java-mysql/src/main/java/com/example/groupproject/models/CustomerRepository.java
+++ server-java-mysql/src/main/java/com/example/groupproject/models/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.example.groupproject.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+	Customer findByCustomerID (Long id);
 }
