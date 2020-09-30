3e0993870167a448e6c5b14e17ea33b1d30a9f76
Jeff <jeffhals569@hotmail.com>
2020-09-30 16:06:58 -0400

    add jeff file to match local

---
+++ client-react/src/pages/Biller.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import axios from "axios";
+import '../index.css';
+import '../task.min.css';
+import Header from '../components/Header';
+
+class Biller extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { Biller: [] };
+    this.billerId = React.createRef();
+    this.accountnumber = React.createRef();
+    this.amount = React.createRef();
+    this.billername = React.createRef(); 
+    this.date = React.createRef();
+    this.paymentType = React.createRef();
+  }
+  
+  // componentDidMount() {
+  //   this.getData();
+  // }
+
+  getData = () => {
+    console.log("In getData");
+    // Java Spring Boot uses port 8080
+    let url = "http://localhost:8080/billers";
+    // C# dotnetcore uses port 5000
+    //let url = "http://localhost:5000/projects";
+    // Express uses port 3001 (react uses 3000)
+    //let url = "http://localhost:3001/tasks";
+    axios.get(url).then(response => this.setState({ Biller: response.data }));
+  //  this.testAll();
+  };
+
+  addBiller = () => {
+    console.log("In addBiller");
+    // this.testAll();
+    let url = "http://localhost:8080/billers";
+    axios.post(url, { accountnumber: this.accountnumber.current.value, amount: this.amount.current.value, billername: this.billername.current.value, date: this.date.current.value, paymentType: this.paymentType.current.value } ).then(_response => {
+      // refresh the data
+      console.log("After axios function call");
+      // this.testAll();
+      this.getData();
+      // empty the input
+      this.emptyInput();
+    });
+  };
+
+  emptyInput(){
+      this.accountnumber.current.value = "";
+      this.amount.current.value = "";
+      this.billername.current.value = "";
+      this.date.current.value = "";
+      this.paymentType.current.value = "";
+  };
+  
+//   testAll(){
+//     console.log("In testAll");
+//     if(this.accountnumber.current.value !== ""){
+//       console.log("accountnumber: " + this.accountnumber.current.value);}
+//     else{
+//       console.log("accountnumber is NULL");
+//     }
+//     if(this.amount.current.value !== ""){
+//         console.log("amount: " + this.amount.current.value);}
+//     else{
+//         console.log("amount is NULL");
+//     }
+//     if(this.billername.current.value !== ""){
+//       console.log("billername: " + this.billername.current.value);}
+//     else{
+//       console.log("billername is null");
+//     }   
+//     if(this.date.current.value !== ""){
+//         console.log("date:" + this.date.current.value);}
+//     else{
+//       console.log("date is null");
+//     } 
+//     if(this.paymentType.current.value !== ""){
+//       console.log("paymentType:" + this.paymentType.current.value);}
+//     else{
+//       console.log("paymentType is null");
+//     }
+// };
+
+  render() {
+    return (
+      <div id="inputs">
+        <h2>Bill Payment and Budget Tool</h2>
+        <h3>Add Biller</h3>
+        <label> Account Number:
+        <input className="form-control" ref={this.accountnumber} type="number" />
+        </label>
+        <br></br>
+        <label> Biller Name:
+        <input className="form-control" ref={this.billername} type="text" />
+        </label>
+        <br></br>
+        <label> Bill Amount:
+          <input className="form-control" ref={this.amount} type="number" />
+        </label>
+        <br></br>
+        <label> Bill Date :
+          <input className="form-control" ref={this.date} type="date" />
+        </label>
+        <br></br>
+        <label> Payment Type :
+          <input className="form-control" ref={this.paymentType} type="text" />
+        </label>
+        <br></br>
+        <button type="button" className="btn btn-primary" onClick={this.addBiller}>Create</button>
+        <br></br>
+      </div>
+    );
+  }
+}
+export default Biller;
--- client-react/src/pages/User.js
+++ client-react/src/pages/User.js
@@ -7,17 +7,11 @@ import Header from '../components/Header';
 
 
 
-// const Header = () => (
-//   <div>
-//     <h2>Register Account Here</h2>
-//   </div>
-// );
 
 class User extends React.Component{
   constructor(props) {
     super(props);
     this.state = { User: [] };
-    // this.username = React.createRef();
     this.email = React.createRef();
     this.password = React.createRef();
     this.lastname = React.createRef();
@@ -44,10 +38,8 @@ class User extends React.Component{
   addUser = () => {
     let url = "http://localhost:8080/users";
     axios.post(url, {email: this.email.current.value, password: this.password.current.value, lastname: this.lastname.current.value, firstname: this.firstname.current.value } ).then(_response => {
-      // refresh the data
+ 
       this.getData();
-      // empty the input
-      // this.username.current.value = "";
       this.email.current.value = "";
       this.password.current.value = "";
       this.firstname.current.value = "";
@@ -70,6 +62,8 @@ class User extends React.Component{
   render() {
 
     return (
+      <div>
+        <Header />
       <div id="inputs">
         <h4>Register Account Here</h4>
         <h2>Bill Payment and Budget Tool</h2>
@@ -77,10 +71,6 @@ class User extends React.Component{
         <label> Email Address:
         <input className="form-control" ref={this.email} type="text" />
         </label>
-        {/* <br></br>
-        <label> Username:
-        <input className="form-control" ref={this.username} type="text" />
-        </label> */}
         <br></br>
         <label> Password:
         <input className="form-control" ref={this.password} type="password" />
@@ -95,21 +85,15 @@ class User extends React.Component{
         </label>
         <br></br>
         <button type="button" className="btn btn-primary" onClick={this.addUser}>Create</button>
-        <br></br>
-        {/* <label> Email Address to Remove:
-        <input className="form-control" ref={this.username} type="text" />
-        <label/> */}
-        {/* <button type="button" className="btn btn-danger" onClick={this.removeUser}>Delete Account</button> */}
+    
         <ul>
           {this.state.User.map(p => (
-            <li key={p.userid}>  
-            {/* THIS IS "COMPLETE" Task DATABASE FIELD-Sheila note */}
-              {/* {p.username} , { p.password ? "complete" : "not complete" }  */}
-              <button type="button" className="btn btn-success">Complete</button>
+            <li key={p.userid}> 
             //   <button type="button" className="btn btn-danger"onClick={this.removeUser}>Delete</button>
             // </li>
           ))}
         </ul>
+      </div>
       </div>
     );
     
@@ -119,6 +103,4 @@ class User extends React.Component{
 
 
 export default User;
-
-
 
